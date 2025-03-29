57f8b8f195769ba190b4b01c80297f8f3c64c6d9
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-03-29 23:11:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg3" {
-  name     = "my-resource-group1"
-  location = "East US"
-}
 
-resource "azurerm_resource_group" "rg1" {
-  name     = "rahul"
-  location = "East US"
-}
 
 
