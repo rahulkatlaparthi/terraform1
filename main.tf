6005e0dd9c07496370bd2494935c375917edf0c6
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-03-30 11:55:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_public_ip" "example" {
   name                = "example-public-ip"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Network Interface
