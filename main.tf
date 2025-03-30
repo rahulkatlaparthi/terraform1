7bfa691be6968c1631aae2eec06feab72cf80831
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-03-30 11:53:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,15 @@ resource "azurerm_resource_group" "example" {
   location = "East US"
 }
 
-# Network Interface (No Public IP)
+# Public IP
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+# Network Interface
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
   location            = azurerm_resource_group.example.location
@@ -17,11 +25,9 @@ resource "azurerm_network_interface" "example" {
   ip_configuration {
     name                          = "internal"
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
-
-
-
 
 # Windows Virtual Machine
 resource "azurerm_windows_virtual_machine" "example" {
@@ -30,7 +36,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   location            = azurerm_resource_group.example.location
   size                = "Standard_B2s"
   admin_username      = "adminuser"
-  admin_password      = "P@ssw0rd123!"
+  admin_password      = "P@ssw0rd123!"  # Change this password
   network_interface_ids = [azurerm_network_interface.example.id]
 
   os_disk {
@@ -46,4 +52,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   }
 }
 
-
+# Output Public IP Address
+output "public_ip" {
+  value = azurerm_public_ip.example.ip_address
+}
