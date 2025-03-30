4f782c40048938f196e1189f4837c13cf0af8950
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-03-30 11:36:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,48 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
 
+# Network Interface (No Public IP)
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
+  ip_configuration {
+    name                          = "internal"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
+# Windows Virtual Machine
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-win-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd123!"
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+# Output Private IP Address
+output "private_ip" {
+  value = azurerm_network_interface.example.ip_configuration[0].private_ip_address
+}
