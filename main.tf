1d919b5d29a000018192d81de367a42cc80d3fb9
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-04-24 14:42:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "exagmple" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
